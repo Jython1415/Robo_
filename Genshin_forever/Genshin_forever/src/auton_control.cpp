c3ac0c24c499d2d6f7d29756d544f08f16c744ac
Nanolife09 <59468666+Nanolife09@users.noreply.github.com>
2021-04-14 16:41:37 +0900

    temp

--- Genshin_forever/Genshin_forever/src/auton_control.cpp
+++ Genshin_forever/Genshin_forever/src/auton_control.cpp
@@ -1,17 +1,71 @@
 #include "vex.h"
 #include "pragma.h"
+#include "driver_control.h"
 #include "auton_control.h"
 #include <thread>
 
-const float fb_constant = 0.1;
-float power = 0;
-
-void right (float target) {
-  power = rightfront.rotation(rotationUnits::raw) * fb_constant;
-  if (power >= 100) {
-    power = 100;
+void left_movement (float target, int power, bool intake = true) {
+  if (target > 0) {
+    while (leftfront.rotation(rotationUnits::raw) < target) {
+      spin(leftfront, power);
+      spin(leftback, power);
+      if (intake) {
+        spin(h_intake_r);
+        spin(h_intake_l);
+        spin(v_intake_bottom);
+      }
+    }
   }
-  
+  else {
+    while (leftfront.rotation(rotationUnits::raw) > target) {
+      spin(leftfront, -power);
+      spin(leftback, -power);
+      if (intake) {
+        spin(h_intake_r);
+        spin(h_intake_l);
+        spin(v_intake_bottom);
+      }
+    }
+  }
+  leftfront.stop();
+  leftback.stop();
+  if (intake) {
+    h_intake_r.stop();
+    h_intake_l.stop();
+    v_intake_bottom.stop();
+  }
+}
+
+void right_movement (float target, int power, bool intake = true) {
+  if (target > 0) {
+    while (rightfront.rotation(rotationUnits::raw) < target) {
+      spin(rightfront, power);
+      spin(rightback, power);
+      if (intake) {
+        spin(h_intake_r);
+        spin(h_intake_l);
+        spin(v_intake_bottom);
+      }
+    }
+  }
+  else {
+    while (rightfront.rotation(rotationUnits::raw) > target) {
+      spin(rightfront, -power);
+      spin(rightback, -power);
+      if (intake) {
+        spin(h_intake_r);
+        spin(h_intake_l);
+        spin(v_intake_bottom);
+      }
+    }
+  }
+  rightfront.stop();
+  rightback.stop();
+  if (intake) {
+    h_intake_r.stop();
+    h_intake_l.stop();
+    v_intake_bottom.stop();
+  }
 }
 
 void pre_auton() {
@@ -19,6 +73,8 @@ void pre_auton() {
 }
 
 void Hutao_skill() {
+  //thread t1 = thread(left_movement,1000);
+
   /* red
     forward_and_intake(rotation);
     left(rotation);
